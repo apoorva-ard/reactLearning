c7aa7b813a8471e09d612ea5b385dfda2db71537
Apoorva <apoorvaduraisamy@gmail.com>
2020-07-05 20:51:03 +0530

    Components part 1 A:media lists stateIntro

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './Components/MenuComponent';
 
-function App() {
-  return (
-    <div className="App">
-        <Navbar dark color="primary">
-            <div className="container">
-                <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <Navbar dark color="primary">
+                    <div className="container">
+                        <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+                    </div>
+                </Navbar>
+                <Menu/>
             </div>
-        </Navbar>
-    </div>
-  );
+        );
+    }
 }
-
 export default App;
