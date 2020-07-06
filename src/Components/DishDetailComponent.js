1d03922bcfdb28538443bb2ba692ec3f79adb80a
Apoorva <apoorvaduraisamy@gmail.com>
2020-07-06 12:10:34 +0530

    Assignment 1 A:DishDetailComponent

---
+++ src/Components/DishDetailComponent.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import { Media } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+
+class DishDetail extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    formatDate(date) {
+        const option = { year: 'numeric', month: 'short', day: 'numeric' };
+        const d = new Date(date)
+        const newdate = d.toLocaleDateString("en-US", option)
+        return newdate;
+    }
+
+    renderComments(comments) {
+        if (comments == null) {
+            return (
+                <div></div>
+            )
+        }
+        else {
+            const cmnts = comments.map((cmnt) => {
+                return (
+                    <div key={cmnt.id}>
+                        <p>{cmnt.comment}</p>
+                        <p> -- {cmnt.author}, {this.formatDate(cmnt.date)}</p>
+                    </div>
+                )
+            })
+            return (
+                <div className="list-unstyled">
+                    {cmnts}
+                </div>
+            )
+        }
+
+    }
+
+    renderDish(dish) {
+        if (dish != null)
+            return (
+                <React.Fragment>
+                    <div className="col-12 col-md-5 m-1">
+                        <Card>
+                            <CardImg top src={dish.image} alt={dish.name} />
+                            <CardBody>
+                                <CardTitle>{dish.name}</CardTitle>
+                                <CardText>{dish.description}</CardText>
+                            </CardBody>
+                        </Card>
+                    </div>
+                    <div className="col-12 col-md-5 m-1">
+                        <h4>Comments</h4>
+                        {this.renderComments(dish.comments)}
+                    </div>
+                </React.Fragment>
+            );
+        else
+            return (
+                <div></div>
+            );
+    }
+
+    render() {
+        return (
+            <div className="row">
+                {this.renderDish(this.props.selectedDish)}
+            </div>
+        );
+    }
+}
+
+export default DishDetail;
--- src/Components/MenuComponent.js
+++ src/Components/MenuComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Media } from 'reactstrap';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import DishDetail from './DishDetailComponent';
 
 class Menu extends Component {
     constructor(props) {
@@ -51,11 +52,7 @@ class Menu extends Component {
                 <div className="row">
                     {menu}
                 </div>
-                <div className="row">
-                    <div className="col-12 col-md-5 m-1">
-                        {this.renderDish(this.state.selectedDish)}
-                    </div>
-                </div>
+                <DishDetail selectedDish={this.state.selectedDish}/>
             </div>
         );
     }
