20d98fca9a1cd73282716ae884e937f5442b0bd2
Apoorva <apoorvaduraisamy@gmail.com>
2020-07-08 12:29:23 +0530

    Intro to Redux A:store reducer connect mapstatetoprops

--- src/App.js
+++ src/App.js
@@ -2,16 +2,24 @@ import React, { Component } from 'react';
 import Main from './Components/MainComponent';
 import { BrowserRouter } from 'react-router-dom';
 import './App.css'
+import { Provider } from 'react-redux';
+//The <Provider /> makes the Redux store available to any nested components that have been wrapped in the connect() function.
+import { ConfigureStore } from './redux/configureStore';
+
+const store = ConfigureStore();
 
 class App extends Component {
     render() {
         return (
-            <BrowserRouter>
-                <div className="App">
-                    <Main />
-                </div>
-            </BrowserRouter>
+            <Provider store={store}>
+                <BrowserRouter>
+                    <div className="App">
+                        <Main />
+                    </div>
+                </BrowserRouter>
+            </Provider>
         );
     }
 }
 export default App;
+//if we export as default no need to specify paranthesis while importing
