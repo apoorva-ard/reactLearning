7b34ca21684173e5f3b11bfa7d4f2a25be5e3912
Apoorva <apoorvaduraisamy@gmail.com>
2020-07-10 13:20:09 +0530

    Redux thunk and logger A:addMiddleware, dispatch inside thunk function

--- src/Components/DishDetailComponent.js
+++ src/Components/DishDetailComponent.js
@@ -2,16 +2,14 @@ import React, { Component } from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem, Button, Modal, ModalHeader, ModalBody, Row, Label, Col } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { LocalForm, Control, Errors } from 'react-redux-form';
-
-    /*
-    formatDate(date) {
-        const option = { year: 'numeric', month: 'short', day: 'numeric' };
-        const d = new Date(date)
-        const newdate = d.toLocaleDateString("en-US", option)
-        return newdate;
-    }*/
-
-    //another way for formatting date : {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
+import { Loading } from './LoadingComponent'
+/*
+formatDate(date) {
+    const option = { year: 'numeric', month: 'short', day: 'numeric' };
+    const d = new Date(date)
+    const newdate = d.toLocaleDateString("en-US", option)
+    return newdate;
+}*/
 
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
 const minLength = (len) => (val) => val && (val.length >= len);
@@ -41,6 +39,7 @@ function RenderComments({ comments }) {
 }
 
 function RenderDish({ dish }) {
+
     if (dish != null)
         return (
             <React.Fragment>
@@ -138,27 +137,46 @@ class CommentForm extends Component {
 
 
 const DishDetail = (props) => {
-    return (
-        <div className="container">
-            <div className="row">
-                <Breadcrumb>
-                    <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
-                    <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
-                </Breadcrumb>
-            </div>
-            <div className="row">
-                <div className="col-12 col-md-5 m-1">
-                    <RenderDish dish={props.dish} />
-                </div>
-                <div className="col-12 col-md-5 m-1">
-                    <RenderComments comments={props.comments} />
-                    <CommentForm comments={props.comments}
-                        addComment={props.addComment}
-                        dishId={props.dish.id}/>
+    if (props.isLoading) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <Loading />
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
+    else if (props.errMess) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <h4>{props.errMess}</h4>
+                </div>
+            </div>
+        );
+    }
+    else if (props.dish != null) 
+        return (
+            <div className="container">
+                <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                </div>
+                <div className="row">
+                    <div className="col-12 col-md-5 m-1">
+                        <RenderDish dish={props.dish} />
+                    </div>
+                    <div className="col-12 col-md-5 m-1">
+                        <RenderComments comments={props.comments} />
+                        <CommentForm comments={props.comments}
+                            addComment={props.addComment}
+                            dishId={props.dish.id}/>
+                    </div>
+                </div>
+            </div>
+        );
 }
 
 export default DishDetail;
